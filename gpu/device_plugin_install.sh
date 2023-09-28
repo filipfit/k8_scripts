0dbaecbfc9441be352c087dbd86c72f25e920d05
filipfit <filipfitzermann@gmail.com>
2023-09-28 12:00:03 +0200

    Updated enable gpu script

--- gpu/device_plugin_install.sh
+++ gpu/device_plugin_install.sh
@@ -1,3 +1,6 @@
+#! /usr/bin/bash
+# Taken from https://github.com/NVIDIA/k8s-device-plugin#deployment-via-helm
+
 helm repo add nvdp https://nvidia.github.io/k8s-device-plugin
 helm repo update
 helm upgrade -i nvdp nvdp/nvidia-device-plugin \
